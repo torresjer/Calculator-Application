865a085cea46fddc09ce0e0599c009591afd3a3c
Jeremy Torres <JCTorres1@student.fullsail.edu>
2022-05-09 18:26:40 -0400

    Created the basics for the MainWindow class

---
+++ Calculator Application/MainWindow.cpp
@@ -0,0 +1,3 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30,30), wxSize(1200,600)){}
---
+++ Calculator Application/MainWindow.h
@@ -0,0 +1,16 @@
+//This is the Main window of the application
+#pragma once
+#include "wx/wx.h"
+
+class MainWindow : public wxFrame //ensure that you are inheriting from wxFrame 
+{
+public:
+	MainWindow();
+	~MainWindow();
+
+public:
+
+
+
+};
+
