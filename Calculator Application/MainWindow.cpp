0925ed07dad03df41d719b6c14c7a7d0f2ce5466
Jeremy Torres <JCTorres1@student.fullsail.edu>
2022-05-16 00:45:56 -0400

    Implemented events and adjusted button initialization.

--- Calculator Application/MainWindow.cpp
+++ Calculator Application/MainWindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 
 	long style = wxTE_RIGHT | wxTE_READONLY;
 	int buttonLable = 0;
 	wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	
 
 	wxBoxSizer* mainWindowSizer = new wxBoxSizer(wxVERTICAL);
 
@@ -17,36 +18,110 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 
 	mainWindowSizer->Add(outputWindow, 0, wxEXPAND | wxTOP | wxBOTTOM);
 	
 	//Created a grid for the calc UI
-
 	wxGridSizer* buttonSelection = new wxGridSizer(fieldWidth, fieldLength, 3, 3);
 	button = new wxButton * [fieldWidth * fieldLength];
 
-	
-	
-
-
-	
-	for(int x = 0; x < fieldLength; x++)
+	for(int x = 0; x < fieldLength * fieldWidth; x++)
 	{
-		for (int y = 0; y < fieldWidth; y++)
-		{
-			button[y * fieldWidth + x] = new wxButton(this, 10000 + (y * fieldWidth + x) , operationLabels[buttonLable], wxPoint(buttonStartingPosX,buttonStartingPosY));
-			button[y * fieldWidth + x]->SetFont(font);
-			buttonSelection->Add(button[y * fieldWidth + x], 1, wxEXPAND | wxALL);
+			button[x] = new wxButton(this, 10000 + x , operationLabels[buttonLable], wxPoint(buttonStartingPosX, buttonStartingPosY));
+			button[x]->SetFont(font);
+			buttonSelection->Add(button[x], 1, wxEXPAND | wxALL);
+
+			button[x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &MainWindow::ButtonSelected, this);
 			buttonLable++;
-		}
-
-		
 	}
-
+	
+	
 	mainWindowSizer->Add(buttonSelection, 1, wxEXPAND);
 
 	this->SetSizer(mainWindowSizer);
 	
+	
 }
 
 void MainWindow::ButtonSelected(wxCommandEvent &event){
+	
+	//Cordinates of button selection.
+	wxFont textBoxFont(32, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+
+	int selectedButtonID = event.GetId() - 10000;
+	outputWindow->SetFont(textBoxFont);
+
+	switch (selectedButtonID)
+	{
+	case 0:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 1:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 2:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 3:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 4:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 5:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 6:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 7:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 8:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 9:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 10:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 11:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 12:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 13:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 14:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 15:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 16:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 17:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 18:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	case 19:
+		writeTextForButtonSelected(selectedButtonID);
+		break;
+	default:
+		break;
+	}
+
+	
+
+
+}
+void MainWindow::writeTextForButtonSelected(int buttonID) {
+
+	outputWindow->AppendText(operationLabels[buttonID]);
 
 }
 
 MainWindow::~MainWindow(){}
+
--- Calculator Application/MainWindow.h
+++ Calculator Application/MainWindow.h
@@ -12,11 +12,6 @@ public:
 	//This is the button field available to the user
 	int fieldWidth = 4;
 	int fieldLength = 5;
-	//The individual button size 
-	//int buttonSizeX = 135;
-	//int buttonSizeY = 107;
-	//int orignalButtonPosX = 20;
-	//int orignalButtonPosY = 220;
 	int buttonStartingPosX = 20;
 	int buttonStartingPosY = 220;
 private:
@@ -24,11 +19,12 @@ private:
 
 	wxSizer* buttonSelection = nullptr;
 	std::string operationLabels[20] = 
-		{ "1","2","3","4","+","5","6","7","-","8","9","0","/","=","C","*","%","Bin","Hex","Dec"};
+		{ "1","2","3","4","+","5","6","7","8","-","9","0","/","=","C","Dec","%","Bin","Hex","*"};
 	wxTextCtrl* outputWindow = nullptr;
 	wxButton** button = nullptr;
 
 	void ButtonSelected(wxCommandEvent &event);
+	void writeTextForButtonSelected(int buttonID);
 
 	wxDECLARE_EVENT_TABLE();
 
