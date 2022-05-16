314aa66de3138daa6e7aad79ee77992096f5436f
Jeremy Torres <JCTorres1@student.fullsail.edu>
2022-05-16 01:13:24 -0400

    Fixed Operation Labels array to match calculator design.

--- Calculator Application/MainWindow.h
+++ Calculator Application/MainWindow.h
@@ -19,7 +19,7 @@ private:
 
 	wxSizer* buttonSelection = nullptr;
 	std::string operationLabels[20] = 
-		{ "1","2","3","4","+","5","6","7","8","-","9","0","/","=","C","Dec","%","Bin","Hex","*"};
+		{ "1","2","3","4","+","5","6","7","8","-","9","0","=","C","/","Dec","Bin","Hex","%","*"};
 	wxTextCtrl* outputWindow = nullptr;
 	wxButton** button = nullptr;
 
