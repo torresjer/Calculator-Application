a5ec63ed636e263a6912e98a906bb9308a3b2948
Jeremy Torres <JCTorres1@student.fullsail.edu>
2022-05-27 22:28:54 -0400

    Created Test project and referenced Calculator App.

---
+++ Calculator ApplicationTest/Calculator ApplicationTest.cpp
@@ -0,0 +1,12 @@
+// Calculator ApplicationTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include "../Calculator Application/Calculator_Processor.h"
+#include "wx/wx.h"
+#include <iostream>
+
+int main()
+{
+    
+    
+}
+
